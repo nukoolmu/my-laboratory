e5d7339137cb68c3f304dd6e5a92debc9e3170d2
Nukool Muangsoon <nukoolmu@ais.co.th>
2023-11-30 14:50:19 +0700

    update

--- run.bash
+++ run.bash
@@ -11,8 +11,15 @@ SCRIPT_NAME=$(basename ${0})
 SCRIPT_HOME=$(echo "${SCRIPT_PATH}"|sed "s|/${SCRIPT_NAME}||g")
 PROCESS_ID="$$"
 
+# init script
+HOST_INPUT=$(hostname)
+IPADDR_INPUT=$(ifconfig |grep -A 1 eth|grep inet|awk -F':' '{print $2}'|awk '{print $1}')
+
+# custom configure
+OUT_PUTFILE=${SCRIPT_HOME}/result/${HOST_INPUT}_${IPADDR_INPUT}_$(date +'%Y%m$d_%H%M').csv
+
 # Include Profile
-read -ra PROFILE_LIST <<< $(find ${SCRIPT_HOME}/ -type f -name *.cnf)
+read -ra PROFILE_LIST <<< $(find ${SCRIPT_HOME}/ -type f -name "*.cnf")
 
 # Valid profile exist
 if [ ${#PROFILE_LIST} -eq 0 ];then
@@ -20,8 +27,10 @@ if [ ${#PROFILE_LIST} -eq 0 ];then
     exit 127
 fi
 
+
+
 # include all function
-for ifunc in $(find ${SCRIPT_HOME}/bin/  -type f) ;do
+for ifunc in $(find ${SCRIPT_HOME}/bin/  -type f -name *.fn) ;do
     . $ifunc
 done
 
@@ -31,4 +40,3 @@ menuDisplay
 
 # exec script as selective profile
 . ${SCRIPT_HOME}/etc/$PROFILE
-
